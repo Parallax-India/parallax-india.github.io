fdc815c222d7f8803846e4949c7052d4b7502666
Prashant Hazariwal <prashantHazariwal999@gmail.com>
2025-02-09 20:25:36 +0530

    converted to php extentions and used include to import files

---
+++ partials/footer.php
@@ -0,0 +1,43 @@
+<footer id="mainContainer">
+    <div class="upper-container">
+      <div class="upper-left">
+        <div class="logo">
+          <img src="assets/logo.png" alt="Parallax Logo" />
+        </div>
+        <p>Uncover the potency <br />of Parallax at</p>
+        <h2>info@parallaxindia.in</h2>
+      </div>
+      <div class="upper-right">
+        <div class="get-started">
+          <h3>Get Started</h3>
+          <button class="contact-button">
+            Go
+            <i class="ri-arrow-right-line"></i>
+          </button>
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+    <div class="mid-container">
+      <div class="left">
+        <h2>contact us</h2>
+        <span> <i class="ri-whatsapp-line"></i> +91-8448324434 </span>
+        <h3>Privacy Policy</h3>
+        <h3>Terms and Conditions</h3>
+        <h3>Return/Refund Policy</h3>
+        <h3>Shipping and Return Policy</h3>
+      </div>
+      <div class="right">
+        <h2>address</h2>
+        <h3>Sector-82 Faridabad</h3>
+        <h3>Haryana - 121002</h3>
+        <span><i class="ri-facebook-line"></i>
+          <i class="ri-instagram-line"></i>
+          <i class="ri-linkedin-line"></i></span>
+      </div>
+    </div>
+    <div class="footer-text">
+      <h5>© 2025 Parallax India, All Rights Reserved</h5>
+    </div>
+  </footer>
---
+++ services.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>services</title>
+    <?php include('partials/head.php') ; ?>
+</head>
+<body>
+    <h1>welcome to service page</h1>
+    <?php include('partials/footer.php') ; ?>
+  <?php include('partials/scripts.php') ; ?>
+</body>
+</html>
